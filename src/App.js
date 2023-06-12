0a8b45a599996c3f18265325ab49027e0c13a02f
EtishaGarg <gargetisha@gmail.com>
2023-06-12 19:59:36 +0530

    Like feature added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const count=0;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='like-button'>
+        <div className='heart-bg'>
+          <div className='heart-icon'></div>
+          </div>
+          <div className='likes-amount'>
+            {count}
+          </div>
+      </div>
     </div>
   );
-}
+} 
 
 export default App;
