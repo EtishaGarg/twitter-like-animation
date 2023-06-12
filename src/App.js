2dab79db15b6f270b4f18cb5ac51a95428f71679
EtishaGarg <gargetisha@gmail.com>
2023-06-12 20:02:45 +0530

    Count toggle feature added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const count=0;
+  const [likeCount, setLikeCount] = useState(0);
+  const [liked, setLiked] = useState(false);
+
+  const toggleDisplay = () => {
+    if(likeCount === 0){
+      setLikeCount(likeCount+1);
+      setLiked(true);
+    } else {
+      setLikeCount(likeCount-1);
+      setLiked(false);
+    }
+  };
   return (
     <div className='App'>
       <div className='like-button'>
         <div className='heart-bg'>
-          <div className='heart-icon'></div>
+          <div className={`heart-icon ${liked ? 'liked' : ''}`} onClick={toggleDisplay}></div>
           </div>
           <div className='likes-amount'>
-            {count}
+            {likeCount}
           </div>
       </div>
     </div>
